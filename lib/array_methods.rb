f0e0da9e7fb0603e7c4c4a8e0ea1d9ab677bd3fd
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 01:08:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,7 +7,7 @@ def find_element_index(array, value_to_find)
       p nil
     end}
 end
-find_element_index([1,2,3], 2)
+find_element_index(,)
 
 def find_max_value(array)
   # Add your solution here
