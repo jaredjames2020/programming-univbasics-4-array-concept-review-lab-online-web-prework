05e36f71ce8ebed7d6b833b27a6aff66fe46ffae
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 01:10:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,30 +1,35 @@
-def Dinding_data
+def Finding Data()
+  scale = [2,3,4,5]
+  scale_2 = [6,5,4,3]
+  hill = [1,2,3,4,5,4,3,2,1]
+  valley = [5,4,3,2,1,0,1,2,3,4,5]
+  zig_zag = [500,4,1000,5,250]
 
-def find_element_index(array, value_to_find)
-  # Add your solution here
-  array.each { |a|
-    if array.include?(value_to_find) == true
-      p array.index(value_to_find)
-    else 
-      p nil
-    end}
-end
-find_element_index(,)
+    def find_element_index(array, value_to_find)
+      # Add your solution here
+      array.each { |a|
+        if array.include?(value_to_find) == true
+          p array.index(value_to_find)
+        else 
+          p nil
+        end}
+    end
+    find_element_index(,)
 
-def find_max_value(array)
-  # Add your solution here
-  array.length.times do |index|
-    puts array.max
-  end
-end
-find_max_value(scale)
-
-def find_min_value(array)
-  # Add your solution here
-   array.length.times do |index|
-    puts array.min
-  end
-end
+    def find_max_value(array)
+      # Add your solution here
+      array.length.times do |index|
+        puts array.max
+      end
+    end
+    find_max_value(scale)
+    
+    def find_min_value(array)
+      # Add your solution here
+       array.length.times do |index|
+        puts array.min
+      end
+    end
 
 scale = [2,3,4,5]
   scale_2 = [6,5,4,3]
