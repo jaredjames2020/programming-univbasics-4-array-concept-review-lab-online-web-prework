923dd7e3585386fdc9839bab3017e50c510c631a
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 01:33:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,8 @@
-def finding_data
   scale = [2,3,4,5]
   scale_2 = [6,5,4,3]
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
-  
-  data_array = [scale, scale_2, hill, valley, zig_zag]
 
     def find_element_index(array, value_to_find)
       # Add your solution here
@@ -49,4 +46,3 @@ def finding_data
     find_min_value(hill)
     find_min_value(valley)
     find_min_value(zig_zag)
-end
