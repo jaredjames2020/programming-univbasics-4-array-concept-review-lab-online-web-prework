15b445647b68371ae804ade3f12339c8353ed08a
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-12 01:11:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,6 +4,8 @@ def Finding Data()
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
   zig_zag = [500,4,1000,5,250]
+  
+  data_array = [scale, scale_2, hill, valley, zig_zag]
 
     def find_element_index(array, value_to_find)
       # Add your solution here
@@ -14,7 +16,7 @@ def Finding Data()
           p nil
         end}
     end
-    find_element_index(,)
+    find_element_index(data_array,)
 
     def find_max_value(array)
       # Add your solution here
@@ -31,8 +33,4 @@ def Finding Data()
       end
     end
 
-scale = [2,3,4,5]
-  scale_2 = [6,5,4,3]
-  hill = [1,2,3,4,5,4,3,2,1]
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  zig_zag = [500,4,1000,5,250]
+end
